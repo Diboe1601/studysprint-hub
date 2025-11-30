f8755aab2a21c6e7d0c3e0de1d90049b51667b70
SelloMpho <202324276@spu.ac.za>
2025-11-30 10:32:07 +0200

    Fix: force Vite to include sitemap.xml in final build

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,19 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  publicDir: "public",
+  build: {
+    copyPublicDir: true,
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
+
